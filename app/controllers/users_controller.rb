4e0e3dedfd6b6d9a46bcbdfd731ba5abe1df13b9
melzreal <melissa.real@hotmail.com>
2018-12-16 17:10:10 +0000

    user journey controllers and views done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,40 @@
 class UsersController < ApplicationController
 
 	get '/signup' do
-		erb :signup
+	 session.clear
+	 
+	 if logged_in?
+		redirect '/holidays'
+	 else
+		erb :'users/signup'
+	 end
 	end
 
 	post '/signup' do
-	  user = User.new(:username => params[:username], :password => params[:password])
-		if user.save
-			redirect '/login'
-		else
-			redirect '/error'
-		end
+	 if params[:user][:username].empty? || params[:user][:email].empty? || params[:user][:password].empty?
+      redirect '/error'
+     else
+       @user = User.create(params[:user])
+       @user.save
+       session[:user_id] = @user.id
+       redirect '/holidays'
+     end
 	end
+ 
+    
+    get '/login' do
+      if logged_in?
+        redirect '/holidays'
+      else
+        erb :'/users/login'
+      end
+    end
 
 
 	post '/login' do
-	  user = User.find_by(:username => params[:username])	 
-		if user && user.authenticate(params[:password])
+		
+	  user = User.find_by(:username => params[:user][:username])	 
+		if user && user.authenticate(params[:user][:password])
 			session[:user_id] = user.id
 			redirect '/holidays'
 		else
@@ -24,21 +42,14 @@ class UsersController < ApplicationController
 		end
 
 	end
-
-	get '/success' do
-		if logged_in?
-		  erb :success
-		else
-		  redirect '/login'
-		end 
-	end
+	
 
 	get '/error' do
 		erb :'/users/error'
 	end
 
 	get '/logout' do
-		if is_logged_in?(session)
+		if logged_in?
 	      session.clear
 	      redirect '/login'
 	    else
